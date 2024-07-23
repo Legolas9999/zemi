49ad91b53ac82670bfee1dd51bf5b352db62d785
Legolas9999 <1301676432@qq.com>
2024-07-23 17:27:58 +0900

    update

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import time
 import os
-
+import functools
 
 
 # def func(name):
@@ -91,5 +91,40 @@ import os
 # cpu_count = os.cpu_count()
 # print(f"プロセッサ数: {cpu_count}")
 
+####################################################################
 
 
+
+# event trigger function
+def trigger(event):
+    print('EVENT SET')
+    event.set() # wake up coroutines waiting
+
+# event consumer_a
+async def consumer_a(event):
+    consumer_name = 'Consumer A'
+    print('{} waiting'.format(consumer_name))
+    await event.wait()
+    print('{} triggered'.format(consumer_name))
+
+# event consumer_b
+async def consumer_b(event):
+    consumer_name = 'Consumer B'
+    print('{} waiting'.format(consumer_name))
+    await event.wait()
+    print('{} triggered'.format(consumer_name))
+
+# event
+event = asyncio.Event()
+
+# wrap coroutines in one future
+main_future = asyncio.wait([consumer_a(event), consumer_b(event)])
+
+# event loop
+event_loop = asyncio.get_event_loop()
+event_loop.call_later(2, functools.partial(trigger, event)) # trigger event in 0.1 sec
+
+# complete main_future
+done, pending = event_loop.run_until_complete(main_future)
+
+####################################################################
